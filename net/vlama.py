859b14c994e1f30deecc1fcde4944ca47f5d1a28
rishiiyer01 <iyer.ris@northeastern.edu>
2024-07-31 11:30:04 -0400

    image tokenizer and embedder imported successfully from chameleon

--- net/vlama.py
+++ net/vlama.py
@@ -9,16 +9,31 @@ from transformers import fortnite
 from transformers import AutoTokenizer, AutoModelForCausalLM
 
 
-class imageTokenizer():
-
-    def tokenize(image):
-        #tokenizer needs to be altered
-        processor = ChameleonProcessor.from_pretrained("facebook/chameleon-7b")
-        tokens = processor(prompt, images=image, return_tensors="pt").to(model.device, torch.bfloat16)
+class imageEmbedder(nn.Module):
+     def __init__(self):
+        super().__init__()
+        vqmodel = ChameleonForConditionalGeneration.from_pretrained("facebook/chameleon-7b", torch_dtype=torch.bfloat16, device_map="cuda:0")
+        for param in self.VqVae.paramaters():
+            param.requires_grad = False #purposely not finetuning the vision tokenizer
+        self.proj=nn.Linear(256,4096) #projection to embedding size of llama3.1
+    def tokenize(self,tokens):
+        
         # Shift the tokens
         shifted_tokens = [token + 128256 for token in original_tokens]
         
         return shifted_tokens
+    def forward(self,image):
+        encoded=vqmodel.encoder(image)
+        encoded=vqmodel.quant_conv
+        quantized,_,indices=vqmodel.quantize(encoded)
+        tokens=self.tokenize(indices) 
+        b, c, h, w = quantized.shape
+        quantized= quantized.reshape(b, c, h*w)
+        quantized= quantized.permute(0, 2, 1)
+        embedding=self.proj(quantized) #returns tensor of shape (batch,seq_len,embed_dim), seq_len is the number of quantized patches returned from the convolutional encoder
+        return embedding, tokens
+        
+        
 
 
 
